61fce2771c4ec42f73c2b429a745860ae801525d
Ragaz <adeyanjukolawole70@gmail.com>
2022-06-23 19:17:53 -0700

    This is my first javasript assignment

---
+++ assignment.js
@@ -0,0 +1,9 @@
+let name = 'Kolawole'
+let height = '5"'
+let country = 'Nigeria'
+
+console.log("my name is " + name)
+
+console.log("i am " + height + "tall")
+
+console.log("i am from " + country)
